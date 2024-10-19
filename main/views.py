655a1ac261e2a4d2c2905a8a347afd05466758b0
Rodion <misterkrv777@yandex.ru>
2024-10-19 16:36:03 +0300

    Views remaked with CBV

--- main/views.py
+++ main/views.py
@@ -1,22 +1,44 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 from goods.models import Categories
 
 
-def index(request):
+class IndexView(TemplateView):
+    template_name = "main/index.html"
 
-    context = {
-        "title": "Home",
-        "content": "Магазин мебели OKEA",
-    }
-    return render(request, "main/index.html", context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "OKEA - Главная"
+        context["content"] = "Магазин мебели OKEA"
+        return context
 
 
-def about(request):
-    context = {
-        "title": "О нас",
-        "content": "О нас",
-        "text_on_page": "Текст о том почему этот магазин крутой",
-    }
-    return render(request, "main/about.html", context)
+class AboutView(TemplateView):
+    template_name = "main/about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "OKEA - О нас"
+        context["content"] = "О нас"
+        context["text_on_page"] = "Текст о том почему этот магазин крутой"
+        return context
+
+
+# def index(request):
+
+#     context = {
+#         "title": "Home",
+#         "content": "Магазин мебели OKEA",
+#     }
+#     return render(request, "main/index.html", context)
+
+
+# def about(request):
+#     context = {
+#         "title": "О нас",
+#         "content": "О нас",
+#         "text_on_page": "Текст о том почему этот магазин крутой",
+#     }
+#     return render(request, "main/about.html", context)
--- orders/urls.py
+++ orders/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 
 app_name = "orders"
 
-urlpatterns = [path("create-order/", views.create_order, name="create_order")]
+urlpatterns = [
+    path("create-order/", views.CreateOrderView.as_view(), name="create_order")
+]
