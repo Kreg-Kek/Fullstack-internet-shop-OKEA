16ef47235d02b8d6b37c325161d640523dcf42f5
Rodion <misterkrv777@yandex.ru>
2024-10-20 18:26:25 +0300

    Caching done

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth, messages
+from django.core.cache import cache
 from django.db.models import Prefetch
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -10,6 +11,7 @@ from django.views.generic import CreateView, TemplateView, UpdateView
 
 
 from carts.models import Cart
+from common.mixins import CacheMixin
 from orders.models import Order, OrderItem
 from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
@@ -74,7 +76,7 @@ class UserRegistrationView(CreateView):
         return context
 
 
-class UserProfileView(LoginRequiredMixin, UpdateView):
+class UserProfileView(LoginRequiredMixin, CacheMixin, UpdateView):
     template_name = "users/profile.html"
     form_class = ProfileForm
     success_url = reverse_lazy("users:profile")
@@ -93,7 +95,8 @@ class UserProfileView(LoginRequiredMixin, UpdateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = "OKEA - Кабинет"
-        context["orders"] = (
+
+        orders = (
             Order.objects.filter(user=self.request.user)
             .prefetch_related(
                 Prefetch(
@@ -102,6 +105,10 @@ class UserProfileView(LoginRequiredMixin, UpdateView):
                 )
             )
             .order_by("-id")
+        )
+
+        context["orders"] = self.set_get_cache(
+            orders, f"user_{self.request.user.id}_orders", 60 * 2
         )
         return context
 
